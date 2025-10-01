1772d4eeafa8c8962e66028935f8d457d22d466a
Fernando Vega Negrete <1224100720.fvn@gmail.com>
2025-09-30 20:00:20 -0600

    Update Eje18.java

--- Eje18.java
+++ Eje18.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Concurso;
-
 import java.util.*;
 
-public class Eje18 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String palabra = sc.nextLine();
@@ -29,4 +23,5 @@ public class Eje18 {
 
         System.out.println(resultado.toString());
     }
+
 }
