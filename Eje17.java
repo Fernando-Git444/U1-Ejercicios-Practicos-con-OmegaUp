4cba5cd4beeb7ad4fd5a7a86edbc7a2cbf63bc8f
Fernando Vega Negrete <1224100720.fvn@gmail.com>
2025-09-30 19:59:38 -0600

    Update Eje17.java

--- Eje17.java
+++ Eje17.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Concurso;
-
 import java.util.*;
 
-public class Eje17 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -34,4 +28,5 @@ public class Eje17 {
 
         System.out.println(mediana + " " + indice);
     }
+
 }
