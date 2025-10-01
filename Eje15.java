2fb5f1a7793d1de735ec73e5dafcf0dbd724de24
Fernando Vega Negrete <1224100720.fvn@gmail.com>
2025-09-30 19:50:14 -0600

    Update Eje15.java

--- Eje15.java
+++ Eje15.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Concurso;
-
 import java.util.Scanner;
 
-public class Eje15 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -19,4 +13,5 @@ public class Eje15 {
 
         System.out.println(suma);
     }
+
 }
