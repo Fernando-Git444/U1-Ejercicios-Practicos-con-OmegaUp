e719930292d51d6382fea75cf20e60bd6f92e342
Fernando Vega Negrete <1224100720.fvn@gmail.com>
2025-09-30 19:41:16 -0600

    Update Eje5.java

--- Eje5.java
+++ Eje5.java
@@ -1,8 +1,6 @@
-package Concurso;
-
 import java.util.Scanner;
 
-public class Eje5 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -30,4 +28,5 @@ public class Eje5 {
             System.out.println("Sigue buscando");
         }
     }
+
 }
